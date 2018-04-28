0823eedf1abd43a73e2853f8d7d7ea67d531be4b
Clint Frandsen <clint.frandsen@gmail.com>
2018-04-28 13:29:09 -0500

    Add files via upload

---
+++ token.cpp
@@ -0,0 +1,31 @@
+#include <sstream>
+#include "token.h"
+#include <string>
+
+using namespace std;
+
+Token::Token(tokenTypeDef token_type, int token_line_num, string token_value){
+    this->tokenType = token_type;
+    this->tokenValue = token_value;
+    this->tokenLineNum = token_line_num;
+};
+string Token::printTokens(){
+    stringstream ss;
+    ss << "(" << tokenType << ",\"" << tokenValue << "\"," << tokenLineNum << ")" << std::endl;
+    string s;
+    //s = to_string(ss);
+    return s;
+};
+
+
+
+
+tokenTypeDef Token::getTokenType(){
+    return tokenType;
+};
+string Token::getTokenValue(){
+    return tokenValue;
+};
+int Token::getTokenLineNum(){
+    return tokenLineNum;
+};
